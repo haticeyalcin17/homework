6cbc09c3f974d321649cb11c86a38ffe190d8c6c
haticeyalcin17 <113948666+haticeyalcin17@users.noreply.github.com>
2022-09-21 17:07:33 +0300

    variables

---
+++ variables/src/variables/variables.java
@@ -0,0 +1,16 @@
+package variables;
+
+public class variables {
+
+	public static void main(String[] args) {
+		int ogrenciSayisi = 10;
+		String mesaj ="Öğrenci sayısı; ";
+		System.out.println(mesaj + ogrenciSayisi);
+		System.out.println("Öğrenci sayım : 9");
+		System.out.println("Öğrenci sayım : 9");
+		System.out.println("Öğrenci sayım : 9");
+		
+		
+	}
+
+}
