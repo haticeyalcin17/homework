82a973d2a1913586f22e021a66c8f723d9f40b93
haticeyalcin17 <113948666+haticeyalcin17@users.noreply.github.com>
2022-09-21 18:06:59 +0300

    datatypes

---
+++ datatypes/src/datatypes/datatypes.java
@@ -0,0 +1,24 @@
+package datatypes;
+
+public class datatypes {
+
+	public static void main(String[] args) {
+		// byte -128 ile 127 arasında değer alır.
+		// int -2147483648 ile +2147483648 arasında değer alır.
+		// double ondalıklı sayılarda kullanılır.
+		// tek karakter tutmak için  char veri tipi kullanılır.Charda sayısal bir veridir.
+		// String ile karakter topluluğu yazılabilir.
+		// boolean true yada false tutan veri tipidir.Şartlı bloklarda kullanılır.
+		
+		int sayi = 12 ;
+		sayi = 12;
+		double sayi1 = 10.4 ;
+		sayi1 = 12;
+		char karakter = 'A';
+		String sehir ="ANKARA";
+		boolean dogruMu=false;
+		if (dogruMu==true) {}
+
+	}
+
+}
