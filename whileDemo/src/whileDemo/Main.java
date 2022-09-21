40fde7b1ae72c0fbf78c985e3d7b7b4037a51d2f
haticeyalcin17 <113948666+haticeyalcin17@users.noreply.github.com>
2022-09-21 19:32:12 +0300

    whileDemo

---
+++ whileDemo/src/whileDemo/Main.java
@@ -0,0 +1,15 @@
+package whileDemo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// while döngüsü
+		int i=1;
+		while(i<10) {
+			System.out.println(i);
+			i++;
+		}
+		System.out.println("While döngüsü bitti");
+	}
+
+}
