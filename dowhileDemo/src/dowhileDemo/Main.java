5eba851df513774befa9c6f5fbd0d785ab19fae3
haticeyalcin17 <113948666+haticeyalcin17@users.noreply.github.com>
2022-09-21 20:27:54 +0300

    dowhileDemo

---
+++ dowhileDemo/src/dowhileDemo/Main.java
@@ -0,0 +1,17 @@
+package dowhileDemo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// do while döngüsü
+		int j=1;
+		do {
+			System.out.println(j);
+			j+=2;	
+		}while(j<10); 
+			
+		System.out.println("Do-While döngüsü bitti");
+
+	}
+
+}
